62fe0b7fd11d0a08e07755ce9617d75772415d4f
Sanjaa46 <sanjaas880@gmail.com>
2026-05-01 18:16:10 +0000

    deploying in docker

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Running database migrations..."
+npx prisma migrate deploy
+
+echo "Running bootstrap..."
+node dist/bootstrap/bootstrap.js
+
+echo "Starting server..."
+node dist/server.js
