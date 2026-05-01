32437b68d18184a8025c35bc414b7c338bad367e
Sanjaa46 <sanjaas880@gmail.com>
2026-05-01 15:57:38 +0000

    Added tests for adminMiddleware

---
+++ src/middleware/userMiddleware.test.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from "express";
+import { vi, describe, it, expect } from 'vitest';
+import { userMiddleware } from "./userMiddleware";
+import jwt from "jsonwebtoken";
+
+describe('userMiddleware', () => {
+    it('should call next() when role is user', () => {
+        // Arrange
+        const req = {
+            // Manually mock what authMiddleware would have produced
+            user: {
+                userId: 1,
+                email: "testuser@example.com",
+                role: "user"
+            }
+        } as unknown as Request;
+
+        const res = {
+            status: vi.fn().mockReturnThis(),
+            json: vi.fn()
+        } as unknown as Response;
+        
+        const next = vi.fn();
+
+        // Act
+        userMiddleware(req, res, next);
+
+        // Assert
+        expect(next).toHaveBeenCalled();
+    })
+
+    it('should return 403 if role is not user', () => {
+        // Arrange
+        const req = {
+            // Manually mock what authMiddleware would have produced
+            user: {
+                userId: 1,
+                email: "testuser@example.com",
+                role: "admin"
+            }
+        } as unknown as Request;
+
+        const res = {
+            status: vi.fn().mockReturnThis(),
+            json: vi.fn()
+        } as unknown as Response;
+        
+        const next = vi.fn();
+
+        // Act
+        userMiddleware(req, res, next);
+
+        // Assert
+        expect(res.status).toHaveBeenCalledWith(403);
+        expect(res.json).toHaveBeenCalledWith({ message: "Only users can access this resource" });
+        expect(next).not.toHaveBeenCalled();
+    })
+})
